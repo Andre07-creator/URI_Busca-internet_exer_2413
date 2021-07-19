341efa41ee94397952782b801376a355bdb352fb
Andre07-creator <aramaldes876@gmail.com>
2021-07-19 20:40:32 -0300

    Starting v1.0

---
+++ URI_Busca-internet_exer_2413/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace URI_Busca_internet_exer_2413
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            if (t >= 1 && t <= 1000)
+            {
+                t *= 2;
+                t *= 2;
+                Console.WriteLine(t);
+            }
+            
+        }
+    }
+}
